5279b1cda0a379c46f82ebcc1c14495d1ba55120
Minh To <toanhminh0412@gmail.com>
2022-11-30 22:07:12 -0800

    Add urls for joining and editing games

--- soccer_app/soccer/urls.py
+++ soccer_app/soccer/urls.py
@@ -3,7 +3,13 @@ from . import views
 from . import invisible_views
 
 urlpatterns = [
-    path('', views.Homepage.as_view(), name='soccer_homepage'),
-    path('create_game/', invisible_views.create_game, name='create_game'),
+    # Visible views
+    path('', views.Dashboard.as_view(), name='dashboard'),
+    path('game/', views.GamesView.as_view(), name='soccer_homepage'),
     path('game/<int:game_id>/', views.GameDetailView.as_view(), name='game_detail'),
+    
+    # Invisible views
+    path('create_game/', invisible_views.modify_game, name='create_game'),
+    path('edit_game/<int:game_id>/', invisible_views.modify_game, name='edit_game'),
+    path('join_game/<int:game_id>/', invisible_views.join_game, name='join_game'),
 ]
