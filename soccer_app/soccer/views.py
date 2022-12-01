a17e38923e8bd085b6c59c0fefaf8b97d106c423
Minh To <toanhminh0412@gmail.com>
2022-11-30 22:07:27 -0800

    Add a view for dashboard

--- soccer_app/soccer/views.py
+++ soccer_app/soccer/views.py
@@ -1,14 +1,31 @@
 # from django.shortcuts import render
-# from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Game
+from .models import Game, User
 
-# Create your views here.
-# Homepage that shows all the currently active game
-class Homepage(ListView):
+# Dashboard: Allows users to manage their games and teams
+class Dashboard(TemplateView):
+    template_name = 'soccer/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Get current user information
+        user_id = int(self.request.session.get('user_id', -1))
+        
+        # Get all games that this user is an organizer
+        context['your_games'] = []
+        for game in Game.objects.all():
+            if len(game.organizers.filter(id=user_id)) > 0:
+                context['your_games'].append(game)
+
+        return context
+
+# GamesView that shows all the currently active game
+class GamesView(ListView):
     model = Game
-    template_name = "soccer/home.html"
+    template_name = "soccer/games.html"
     context_object_name = "all_games"
 
 # A game detail page
