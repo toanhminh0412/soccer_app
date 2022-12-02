97b1e683fbca77ad3e7b06537aeb50333ddaa8d3
Minh To <toanhminh0412@gmail.com>
2022-12-01 22:15:05 -0800

    Add an url for deleting game

--- soccer_app/soccer/urls.py
+++ soccer_app/soccer/urls.py
@@ -5,11 +5,12 @@ from . import invisible_views
 urlpatterns = [
     # Visible views
     path('', views.Dashboard.as_view(), name='dashboard'),
-    path('game/', views.GamesView.as_view(), name='soccer_homepage'),
-    path('game/<int:game_id>/', views.GameDetailView.as_view(), name='game_detail'),
+    path('game', views.GamesView.as_view(), name='soccer_homepage'),
+    path('game/<int:game_id>', views.GameDetailView.as_view(), name='game_detail'),
     
     # Invisible views
-    path('create_game/', invisible_views.modify_game, name='create_game'),
-    path('edit_game/<int:game_id>/', invisible_views.modify_game, name='edit_game'),
-    path('join_game/<int:game_id>/', invisible_views.join_game, name='join_game'),
+    path('create_game', invisible_views.modify_game, name='create_game'),
+    path('edit_game/<int:game_id>', invisible_views.modify_game, name='edit_game'),
+    path('join_game/<int:game_id>', invisible_views.join_game, name='join_game'),
+    path('delete_game/<int:game_id>', invisible_views.delete_game, name='delete_game'),
 ]
