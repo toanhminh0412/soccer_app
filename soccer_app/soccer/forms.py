43f9a9a7051edef8408779ed8725fcbbb1c4ca1b
Minh To <toanhminh0412@gmail.com>
2022-12-03 00:04:33 -0800

    Make a form for groups

--- soccer_app/soccer/forms.py
+++ soccer_app/soccer/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Game
+from .models import Game, Team
 
 # Form for creating a new game
 class GameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ['name', 'team', 'date', 'location', 'max_player_num', 'team_num', 'visible_to_everyone', 'description']
+
+# Form for creating a new team
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = ['name', 'max_member_num', 'description']
