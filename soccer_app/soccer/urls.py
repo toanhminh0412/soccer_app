82c6d1763cb9e305fcd8104f8fe4afc752960c2d
Minh To <toanhminh0412@gmail.com>
2022-12-04 16:04:50 -0800

    Add urls for editing and deleting groups

--- soccer_app/soccer/urls.py
+++ soccer_app/soccer/urls.py
@@ -5,8 +5,12 @@ from . import invisible_views
 urlpatterns = [
     # Visible views
     path('', views.Dashboard.as_view(), name='dashboard'),
+
     path('game', views.GamesView.as_view(), name='games'),
     path('game/<int:game_id>', views.GameDetailView.as_view(), name='game_detail'),
+
+    path('group', views.GroupsView.as_view(), name='groups'),
+    path('group/<int:group_id>', views.GroupDetailView.as_view(), name='group_detail'),
     
     # Invisible views
     path('create_game', invisible_views.modify_game, name='create_game'),
@@ -15,6 +19,6 @@ urlpatterns = [
     path('delete_game/<int:game_id>', invisible_views.delete_game, name='delete_game'),
 
     path('create_group', invisible_views.modify_group, name='create_group'),
-    path('group', views.GroupsView.as_view(), name='groups'),
-    path('group/<int:group_id>', views.GroupDetailView.as_view(), name='group_detail'),
+    path('edit_group/<int:group_id>', invisible_views.modify_group, name='edit_group'),
+    path('delete_group/<int:group_id>', invisible_views.delete_group, name='delete_group'),
 ]
